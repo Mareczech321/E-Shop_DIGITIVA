c4a82e9e394fade52254db0cad41f67b3d103db0
Mareczech <marekmulac14@gmail.com>
2025-05-14 22:46:15 +0200

    Add files via upload

---
+++ PHP/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digitiva</title>
+    <link rel="stylesheet" href="../CSS/login.css">
+    <link rel="shortcut icon" href="../IMG/favicon.png" type="image/x-icon">
+</head>
+<body>
+    <article>
+        <a href="http://digitiva.wz.cz:8080/"><img src="../IMG/Digitiva2.png" id="logo"></a>
+
+        <form method="post" id="login_form">
+            <label for="Username">Username</label><br><input type="text" name="Username" placeholder="  Username"><br>
+            <label for="Password">Password</label><br><input type="password" name="Password" placeholder="  Password"><br>
+            <label for="E-mail">E-mail</label><br><input type="text" name="E-mail" placeholder="  E-mail"><br>
+            <input type="submit" name="login" id="login_submit" value="Login">
+        </form>
+    </article>
+</body>
+</html>
+<?php
+    
+
+    
+?>
