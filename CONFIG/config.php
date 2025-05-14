bc01ae473118cad8f1e2a013e38057d91af85e39
Mareczech <marekmulac14@gmail.com>
2025-05-14 22:45:30 +0200

    Add files via upload

---
+++ CONFIG/config.php
@@ -0,0 +1,8 @@
+<?php 
+
+define('DB_HOST', 'sql5.webzdarma.cz',);
+define('DB_NAME', 'digitivawzcz3246');
+define('DB_USER', 'digitivawzcz3246');
+define('DB_PASSWORD', 'r&9&j&8A%5%RDFSi9M23');
+
+?>
