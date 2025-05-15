49cc72397b29ca5c704a4fb2d66604752e71cafb
Mareczech <marekmulac14@gmail.com>
2025-05-15 21:29:26 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digitiva</title>
+    <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="shortcut icon" href="./IMG/favicon.png" type="image/x-icon">
+</head>
+<body>
+    <nav>
+        <button id="burger_menu"></button>
+
+        <a href="http://marekmulac.wz.cz:8080"><img src="./IMG/Digitiva2.png" id="logo"></a>
+        
+        <form method="post" id="search">
+            <input type="text" name="search" id="search_bar" placeholder="  Search for products">
+            <input type="submit" name="searchDB" id="search_button" value="">
+        </form>
+        <a href="./PHP/login.php" id="login-buttons">Login / Sign up</a>
+    </nav>
+    <article>
+        
+    </article>
+</body>
+</html>
